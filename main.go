765ed090795bf17fbafb94ba48179e62853d7e61
spcameron <cameron.seanpatrick@gmail.com>
2025-08-25 15:20:31 -0400

    REPL updated to register 'exit' command

--- main.go
+++ main.go
@@ -7,14 +7,10 @@ import (
 	"strings"
 )
 
-func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-	for {
-		fmt.Print("Pokedex > ")
-		scanner.Scan()
-		input := cleanInput(scanner.Text())
-		fmt.Printf("Your command was: %s\n", input[0])
-	}
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
 }
 
 func cleanInput(text string) []string {
@@ -26,4 +22,40 @@ func cleanInput(text string) []string {
 	}
 
 	return output
+}
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex ... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func main() {
+	cliCommands := map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		input := cleanInput(scanner.Text())
+		command := input[0]
+
+		cliCommand, ok := cliCommands[command]
+		if !ok {
+			fmt.Println("Unknown command")
+			continue
+		}
+
+		err := cliCommand.callback()
+		if err != nil {
+			fmt.Printf("Error: %v\n", err)
+		}
+	}
 }
