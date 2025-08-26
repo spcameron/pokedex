cb7c03beeac4702c6edf021be452b720ab7b1801
spcameron <cameron.seanpatrick@gmail.com>
2025-08-25 21:18:58 -0400

    Cache Add(), Get(), and reapLoop()

--- internal/pokecache/pokecache.go
+++ internal/pokecache/pokecache.go
@@ -16,24 +16,50 @@ type cacheEntry struct {
 	val       []byte
 }
 
-func NewCache(interval time.Duration) Cache {
-	return Cache{
+func NewCache(interval time.Duration) *Cache {
+	c := &Cache{
+		Entries:  make(map[string]cacheEntry),
 		interval: interval,
+	}
+	go c.reapLoop()
+	return c
+}
+
+func (c *Cache) Add(key string, val []byte) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry := cacheEntry{
+		createdAt: time.Now(),
+		val:       val,
+	}
+	c.Entries[key] = entry
+}
+
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	entry, exists := c.Entries[key]
+	if exists {
+		return entry.val, true
+	} else {
+		return nil, false
 	}
 }
 
-// TODO:
-func (c *Cache) Add(key string, val []byte) {
-	// ...
-}
-
-// TODO:
-func (c *Cache) Get(key string) ([]byte, bool) {
-	// ...
-	return []byte{}, false
-}
-
-// TODO:
 func (c *Cache) reapLoop() {
+	ticker := time.NewTicker(c.interval)
+	for range ticker.C {
+		c.mu.Lock()
+		staleEntries := []string{}
+		for k, v := range c.Entries {
+			if time.Since(v.createdAt) > c.interval {
+				staleEntries = append(staleEntries, k)
+			}
+		}
+		for _, key := range staleEntries {
+			delete(c.Entries, key)
+		}
+		c.mu.Unlock()
+	}
 
 }
